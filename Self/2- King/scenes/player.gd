415257123fa905c995945efeae019725f0578c13
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-06-18 16:28:08 -0300

    king man

---
+++ Self/2- King/scenes/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 200
+const JUMP_FORCE = -300.0
+
+var gravity = 950
+@onready var animation := $anim as AnimatedSprite2D
+
+
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		animation.play("jump")
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_FORCE
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		
+		if is_on_floor():
+			animation.play("run")
+	
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			animation.play("idle")
+		
+	
+	move_and_slide()
+
+
+	
+
+
