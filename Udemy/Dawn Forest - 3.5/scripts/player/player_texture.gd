704b8106422ab557d3a06768b622a2bfe13b050f
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-06-30 16:02:14 -0300

    Updates

--- Udemy/Dawn Forest - 3.5/scripts/player/player_texture.gd
+++ Udemy/Dawn Forest - 3.5/scripts/player/player_texture.gd
@@ -1,13 +1,21 @@
 extends Sprite
 class_name PlayerTexture
 
+var suffix: String = "_right"
+var normal_attack: bool = false
+var shield_off: bool = true
+var crouching_off: bool = true
+
 export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
 export(NodePath) onready var player = get_node(player) as KinematicBody2D
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
+	
+	if player.attacking or player.defending or player.crouching or player.next_to_wall():
+		action_behavior()
 
-	if direction.y != 0:
+	elif direction.y != 0:
 		vertical_behavior(direction)
 	elif player.landing == true:
 		animation.play("landing")
@@ -18,9 +26,17 @@ func animate(direction: Vector2) -> void:
 
 func verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
-		flip_h = false	
+		flip_h = false
+		suffix = "_right"
+		player.direction = -1
+		position = Vector2.ZERO
+		player.wall_ray.cast_to = Vector2(5.5, 0)
 	elif direction.x < 0: # Ignora o 0 para manter o flip para o mesmo lado
 		flip_h = true
+		suffix = "_left"
+		player.direction = 1
+		position = Vector2(-2, 0)
+		player.wall_ray.cast_to = Vector2(-7.5, 0)
 
 
 func horazintal_behavior(direction: Vector2) -> void:
@@ -38,8 +54,28 @@ func vertical_behavior(direction: Vector2) -> void:
 		animation.play("jump")
 
 
+func action_behavior() -> void:
+	if player.next_to_wall():
+		animation.play("wall_slide")
+	elif player.attacking and normal_attack:
+		animation.play("attack" + suffix)
+	elif player.defending and shield_off:
+		animation.play("shield")
+		shield_off = false
+	elif player.defending and crouching_off:
+		animation.play("crouch")
+		crouching_off = false
+
+
 func on_animation_finished(anim_name: String):
 	match anim_name:
 		"landing":
 			player.landing = false
 			player.set_physics_process(true)
+		"attack_left":
+			normal_attack = false
+			player.attacking = false
+		
+		"attack_right":
+			normal_attack = false
+			player.attacking = false
